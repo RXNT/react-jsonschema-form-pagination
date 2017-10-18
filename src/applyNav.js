eb62f08df4f3da2b37b33d8902a02cc4920c5704
Anton Oparin <mavarazy@gmail.com>
2017-10-18 18:53:53 +0300

    fix #29

--- src/applyNav.js
+++ src/applyNav.js
@@ -78,7 +78,11 @@ export default function applyPagination(FormComponent, NavComponent = Navs) {
 
     transformErrors = errors => {
       let errorsWithNav = errors.map(error => {
-        let activeNav = this.navTree.findActiveNav(error.argument);
+        let field =
+          error.property === "instance"
+            ? error.argument
+            : error.property.substring(9);
+        let activeNav = this.navTree.findActiveNav(field);
         if (activeNav && activeNav.length > 0) {
           return Object.assign({ activeNav }, error);
         } else {
