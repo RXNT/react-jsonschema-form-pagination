bf1cc9b6906c0dc8f257a86c5cb03037885af97d
François Boukhalfa <fboukhalfa@viadirect.com>
2022-09-27 15:04:45 +0200

    update to webpack 5

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,9 +1,11 @@
 var path = require("path");
 var webpack = require("webpack");
-var ExtractTextPlugin = require("extract-text-webpack-plugin");
+// var ExtractTextPlugin = require("extract-text-webpack-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
 
 module.exports = {
-    mode: 'development',
+    mode: 'production',
     entry: path.join(__dirname, "playground/app"),
     output: {
         path: path.join(__dirname, "build"),
@@ -15,7 +17,9 @@ module.exports = {
             "process.env": {
                 NODE_ENV: JSON.stringify("production")
             }
-        })
+        }),
+        new MiniCssExtractPlugin()
+
     ],
     resolve: {
       modules: [
@@ -28,7 +32,7 @@ module.exports = {
         rules: [
             {
                 test: /\.jsx?$/,
-                loader: "babel-loader",
+                use: "babel-loader",
                 include: [
                     path.join(__dirname, "src"),
                     path.join(__dirname, "playground"),
@@ -36,8 +40,9 @@ module.exports = {
                 ],
             },
             {
-                test: /\.css$/,
-                loader: ExtractTextPlugin.extract("css-loader"),
+                test: /\.css$/i,
+                // loader: ExtractTextPlugin.extract("css-loader"),
+                use: [MiniCssExtractPlugin.loader, "css-loader"],
                 include: [
                     path.join(__dirname, "playground"),
                     path.join(__dirname, "node_modules"),
